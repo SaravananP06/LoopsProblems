a4f5881383350bd9c6a217a6fc000f82a167810b
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-06 21:04:11 +0530

    [Saravanan]: Added program to print Single digit number names

--- src/Day2/BridgeLabz/IfElseStatementProblems/SingleDigitNumberNames.java
+++ src/Day2/BridgeLabz/IfElseStatementProblems/SingleDigitNumberNames.java
@@ -10,9 +10,37 @@ public class SingleDigitNumberNames {
 
 	public static void main(String[] args) {
 		/*
-		 * Trying to push in git
+		 * Taking input from user
 		 */
-		System.out.println("Welcome to Loops Problems");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any single digit number:- ");
+		int n = sc.nextInt();
+		
+		/*
+		 * For loop
+		 */
+		
+		if (n == 1) {
+			System.out.println("1 - ONE");
+		}else if (n == 2) {
+			System.out.println("2 - TWO");
+		}else if (n == 3) {
+			System.out.println("3 - THREE");
+		}else if (n == 4) {
+			System.out.println("4 - FOUR");
+		}else if (n == 5) {
+			System.out.println("5 - FIVE");
+		}else if (n == 6) {
+			System.out.println("6 - SIX");
+		}else if (n == 7) {
+			System.out.println("7 - SEVEN");
+		}else if (n == 8) {
+			System.out.println("8 - EIGHT");
+		}else if (n == 9) {
+			System.out.println("9 - NINE");
+		}else {
+			System.out.println("Invalid Input!!!");
+		}
 		
 		
 
