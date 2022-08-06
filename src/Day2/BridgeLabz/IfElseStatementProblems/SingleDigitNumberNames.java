4e6348e36ce473c5ac448dc17a064ca07083604c
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-06 20:50:33 +0530

    [Saravanan]: Added code for Welcome Message

---
+++ src/Day2/BridgeLabz/IfElseStatementProblems/SingleDigitNumberNames.java
@@ -0,0 +1,21 @@
+package Day2.BridgeLabz.IfElseStatementProblems;
+
+import java.util.Scanner;
+/*
+ * This program we are taking input for single digit number
+ * Printing the number name
+ */
+
+public class SingleDigitNumberNames {
+
+	public static void main(String[] args) {
+		/*
+		 * Trying to push in git
+		 */
+		System.out.println("Welcome to Loops Problems");
+		
+		
+
+	}
+
+}
